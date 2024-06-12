c3a46300c6f51897bb833d772c48de4586ce6ef7
Rafael Altieris <raphaelaltieris@gmail.com>
2024-06-12 13:17:09 -0300

    Construção da classe para fazer CRUD no SQLite

--- CrudMaui/Helpers/SQLiteDataBaseHelper.cs
+++ CrudMaui/Helpers/SQLiteDataBaseHelper.cs
@@ -1,6 +1,42 @@
-﻿namespace CrudMaui.Helpers
+﻿using SQLite;
+using CrudMaui.Models;
+namespace CrudMaui.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDataBaseHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _conn = new SQLiteAsyncConnection(dbPath);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> Insert(Produto produto)
+        {
+            return _conn.InsertAsync(produto);
+        }
+        public Task<List<Produto>> Update(Produto produto)
+        {
+            string sql = "UPDATE Produto SET Descricao = ?, Quantidade = ?, Preco = ? WHERE Id = ?";
+            
+            return _conn.QueryAsync<Produto>(
+                sql, produto.Descricao, produto.Quantidade, produto.Preco, produto.Id
+            );
+        }
+        public Task<int> Delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i =>i.Id == id);
+        }
+        public Task<List<Produto>> GetAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = "SELECT * FROM Produto WHERE Descricao LIKE '%" + q + "%'";
+            return _conn.QueryAsync<Produto>(
+                sql);
+        }
     }
 }
