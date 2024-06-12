8300bcd38ae6a113c17b4bb4626c3b37af1f2637
Rafael Altieris <raphaelaltieris@gmail.com>
2024-06-12 11:20:58 -0300

    Add models e troca da tela Inicial

--- CrudMaui/Models/Produto.cs
+++ CrudMaui/Models/Produto.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace CrudMaui.Models
 {
-    internal class Produto
+    public class Produto
     {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
     }
 }
